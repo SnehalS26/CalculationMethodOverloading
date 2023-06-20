2067f1f799ba5f61aa1df6adb278e7082a96e58c
SnehalS26 <Snehalshirsath26@gmail.com>
2023-06-20 18:05:30 +0530

    Add project files.

---
+++ CalculationMethodOverloading/Calculations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationMethodOverloading
+{
+    internal class Calculations
+    {
+        public int Cal(int a, int b) 
+        {
+            return a + b;
+        }
+        public double Cal(double x, double y)
+        {
+            return x - y;
+        }
+        public double Cal(int p , double q)
+        {
+            return p * q;
+        }
+
+    }
+}
---
+++ CalculationMethodOverloading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationMethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculations c1 = new Calculations();
+            int result1 = c1.Cal(10, 20);
+            c1.Cal(29.8, 30.7);
+            Console.WriteLine($"Addition = {result1}");
+            double result2 = c1.Cal(2.2, 22.10);
+            Console.WriteLine($"Substraction = {result2}");
+            double result3 = c1.Cal(20, 10.55);
+            Console.WriteLine($"Multiplication = {result3}");
+            
+
+        }
+    }
+}
